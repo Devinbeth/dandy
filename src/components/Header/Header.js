f1ec952827a66f9bce5f043c1e823ef880ec70db
Devin Lethbridge <devinbeth@gmail.com>
2018-03-16 12:08:12 -0600

    saving

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getUser } from '../../ducks/reducer.js';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 class Header extends Component {
-
+    constructor() {
+        super();
+        this.state = {
+            open: false
+        }
+    }
     componentDidMount() {
         this.props.getUser();
     }
@@ -13,9 +20,25 @@ class Header extends Component {
 
         return (
             <div className='Home'>
-                <AppBar title='DANDY'
+                <AppBar 
+                    title='DANDY'
+                    onLeftIconButtonClick={() => this.setState({ open: !this.state.open })}
                     iconElementRight={<a href={process.env.REACT_APP_LOGOUT}><div className='log'>Logout</div></a>}
                 />
+                <Drawer open={this.state.open}>
+                    <AppBar 
+                        onLeftIconButtonClick={() => this.setState({ open: !this.state.open })}
+                    />
+                    <MenuItem>MY CHARACTERS</MenuItem>
+                    <MenuItem>NEW CHARACTER</MenuItem>
+                    <MenuItem>RACES</MenuItem>
+                    <MenuItem>CLASSES</MenuItem>
+                    <MenuItem>WEAPONS</MenuItem>
+                    <MenuItem>ARMOR</MenuItem>
+                    <MenuItem>EQUIPMENT</MenuItem>
+                    <MenuItem>SPELLS</MenuItem>
+                    <MenuItem>MONSTERS</MenuItem>
+                </Drawer>
             </div>
         )
     }
